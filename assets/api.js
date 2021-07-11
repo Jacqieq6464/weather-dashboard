1540bbc6ea94de37adf63d0f107242c4d0adbb10
jacqie williamson <jacqieq@me.com>
2021-07-11 15:57:16 +0100

    API_KEY added to api.js

---
+++ assets/api.js
@@ -0,0 +1,11 @@
+var API_KEY = "4a19d991335886aa3af8b1a2b417e053";
+
+// this is a pure function because it doesn't alter anything else on the page
+function generateURL(city){
+//    future proofing variable enabling the number of days forecasted to be easily changed within the scope of the API
+    var NUMBER_OF_DAYS_TO_FORECAST = 5;
+    return `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_FORECAST}&appid=${API_KEY}`;
+
+}
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
