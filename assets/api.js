fdadf4305c5d65d6c2f0003ca2f6813a03f1a744
jacqie williamson <jacqieq@me.com>
2021-07-11 16:27:47 +0100

    update api.js with callback function

--- assets/api.js
+++ assets/api.js
@@ -5,7 +5,14 @@ function generateURL(city){
 //    future proofing variable enabling the number of days forecasted to be easily changed within the scope of the API
     var NUMBER_OF_DAYS_TO_FORECAST = 5;
     return `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_FORECAST}&appid=${API_KEY}`;
-
 }
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+function requestCity(url){
+    // callback function: when network request comes back with url, run the function below
+    return fetch(url).then(function(response){
+        return response.json()
+    });
+
+}
