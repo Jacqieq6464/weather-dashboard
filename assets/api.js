5c687b6ea49f1e0b1786785acfe16f6a86e5eee3
jacqie williamson <jacqieq@me.com>
2021-07-11 16:39:06 +0100

    NUMBER_OF_DAYS_TO_FORECAST amended to inc https

--- assets/api.js
+++ assets/api.js
@@ -4,7 +4,7 @@ var API_KEY = "4a19d991335886aa3af8b1a2b417e053";
 function generateURL(city){
 //    future proofing variable enabling the number of days forecasted to be easily changed within the scope of the API
     var NUMBER_OF_DAYS_TO_FORECAST = 5;
-    return `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_FORECAST}&appid=${API_KEY}`;
+    return `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_FORECAST}&appid=${API_KEY}`;
 }
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
